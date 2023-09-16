c93d67e9d2004b0f1e13fa737415d0d8261ada7b
nik463 <nik463@gmail.com>
2023-09-16 22:25:04 +0530

    react initialized

---
+++ nullgravity/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className="fontcolor"> 
+      <div>
+    <h1>Hi, This place called NullGravity</h1></div><div >
+    <p1 >Welcome to my digital universe, where technology meets creativity! I go by the name "Null Gravity," a moniker inspired by my deep love for the sky, the universe, the moon, and the wonders of science. I've adopted this name across various social media platforms, and now, I'm thrilled to welcome you to my very own webpage.
+
+My journey began as an electrical engineer, but I've since ventured into the world of software development. With over three years of experience in Python and machine learning, I've harnessed the power of code to bring my ideas to life. The frontend of this page, crafted with care, is a testament to my passion for technology, built on React.
+
+Beyond the realm of coding, I have a deep-seated love for all things audio. As an audiophile, I'm constantly seeking the perfect harmony and sonic beauty in the world of music. But that's not all—I find my escape and inspiration behind the wheel, as I indulge my love for driving, exploring new horizons, and feeling the thrill of the open road.
+
+Join me on this journey through the digital landscape, where the worlds of engineering, software development, and personal passions converge. Together, we'll explore the possibilities of code, the symphony of sound, and the adventures that lie ahead. Welcome to Null Gravity's world; let's defy the limits of what's possible.</p1></div>
+    </div>
+  );
+}
+
+export default App;
